0291e08ee056807755232a526643493990e247f1
sara <sara@DESKTOP-S1BS46T>
2023-10-24 23:18:56 +0900

    [add]practise3_1

---
+++ src/practise/Practise3_1.java
@@ -0,0 +1,17 @@
+package practise;
+
+public class Practise3_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		boolean tenki = true ;
+		if (tenki == true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩に行きます");
+		}else {
+			System.out.println("DVDを見ます");
+		}
+
+	}
+
+}
