fbe1a4541c24e066753b020879c4b10f61f84c55
sara <sara@DESKTOP-S1BS46T>
2023-10-26 16:51:13 +0900

    [add]practise3_4

---
+++ src/practise/practise3_2.java
@@ -0,0 +1,11 @@
+package practise;
+
+public class practise3_2 {
+	public static void main(String[] args) {
+		boolean doorClose = false;
+		while (doorClose == true) {
+			System.out.println("ノックする");
+			System.out.println("1分待つ");
+		}
+	}
+}
