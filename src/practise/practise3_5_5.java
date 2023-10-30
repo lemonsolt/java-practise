e42fa2f933b914c6d073e022a8869dd6d4d1cf1f
sara <sara@DESKTOP-S1BS46T>
2023-10-30 22:59:20 +0900

    [add]practisefile

---
+++ src/practise/practise3_5_5.java
@@ -0,0 +1,28 @@
+package practise;
+
+public class practise3_5_5 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		for(int i = 1; i < 10; i++) {
+			System.out.println(i + "回目");
+		}
+		for(int i = 0; i < 10; i += 2) {
+			System.out.println(i);
+		}
+		
+		for(int i = 10; i > 0; i--) {
+			System.out.println(i);
+		}
+		
+//		for(; i < 10; i++) {
+//			System.out.println(i);
+//		}
+//		
+//		for (int i = 0; i < 10;) {
+//			System.out.println("ループ");
+//		}
+
+	}
+
+}
